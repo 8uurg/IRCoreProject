16f70ccbec93ca0f0f7f65f343731b18e04759c2
Martijn Steenbergen <m.uses+git@outlook.com>
2019-03-13 16:25:01 +0100

    More advanced testing

--- src/test/java/analysis/BaseTest.java
+++ src/test/java/analysis/BaseTest.java
@@ -1,13 +1,13 @@
 package analysis;
 
-import index.IndexFactory;
 import irproject.ICompletionAlgorithm;
-import org.apache.lucene.search.IndexSearcher;
 import org.junit.jupiter.api.Test;
 import reader.DataReader;
 import reader.SearchQuery;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -18,6 +18,9 @@ public abstract class BaseTest {
     private long randomSeed = 123123123;
 
     Random r = new Random(randomSeed);
+    public final static int TESTSIZE = 2000;
+    public final boolean ENABLE_PRINT = true;
+    public final int K = 10;
 
     @Test
     public void Test() throws IOException {
@@ -28,30 +31,96 @@ public abstract class BaseTest {
         DataReader reader = new DataReader(GetFile("user-ct-test-collection-10.txt"));
 
         SearchQuery query;
-        int total = 0;
-        int correct = 0;
+        double MRR = 0;
+        double SIMPLE_RELEVANCE = 0.0;
+        double DCGRELEVANCE = 0.0;
+        double DCGRELEVANCECAPPED = 0.0;
         ICompletionAlgorithm alg = GetAlgorithm();
+        NumberFormat formatter = new DecimalFormat("#0.000");
 
-        for (int i = 0; i < 2000; i++) {
+        for (int i = 0; i < TESTSIZE; i++) {
             query = reader.ReadLine();
-            final String q = query.Query;
-            String[] docs = alg.query(CutOff(query.Query), 10);
-            if(Arrays.asList(docs).contains(q)) {
-                System.out.print("1");
-                correct++;
-            } else {
-                System.out.print("0");
+            final String originalQ = query.Query;
+            String q = CutOff(query.Query);
+            if(q.equals(query.Query)) {
+                continue;
             }
-            total++;
-
-            if(total%100 == 0) {
-                System.out.println();
+            String[] docs = alg.query(q, 10);
+            int index = -1;
+            double mrrChange = 0.0;
+            if((index = Arrays.asList(docs).indexOf(originalQ)) != -1) {
+                if(index <= K) {
+                    mrrChange = 1.0/(index+1);
+                }
             }
 
+            double simpleRelevanceChange = 0.0;
+            double dcgRelevanceChange = 0.0;
+            if (docs.length > 0 && docs[0] != null) {
+                simpleRelevanceChange = calculateRelevance(originalQ, q, docs[0]);
+
+                for (int j = 0; j < docs.length; j++) {
+                    double log = Math.log(j+2) / Math.log(2.0);
+                    dcgRelevanceChange += calculateRelevance(originalQ, q, docs[j]);
+                    if(j > K) {
+                        break;
+                    }
+                }
+
+                double d = 0;
+            }
+
+            MRR += mrrChange;
+            SIMPLE_RELEVANCE += simpleRelevanceChange;
+            DCGRELEVANCE += dcgRelevanceChange;
+            DCGRELEVANCECAPPED += Math.min(dcgRelevanceChange, 1.0);
+
+            Print(padRight(i + ":", 6) + padRight( "MRR: " + formatter.format(mrrChange), 12) + padRight(" RL: " + formatter.format(simpleRelevanceChange), 11) + " RLDCG: " + formatter.format(dcgRelevanceChange));
+            if(Math.abs(mrrChange - simpleRelevanceChange) > 0.2) {
+                Print("  | Large change! originalQ: '" + originalQ + "' q: '" + q + "' docs[0]: '" + docs[0] + "'");
+            }
+
+            if(!ENABLE_PRINT) {
+                if (i % 300 == 0) {
+                    System.out.println(i);
+                }
+            }
         }
-        System.out.println();
-        System.out.println("Correctness Original Metric: " + correct + "/" +total);
+        System.out.println(padRight(TESTSIZE + ":", 6) + padRight( "MRR: " + formatter.format(MRR / TESTSIZE), 13) + padRight("RL: " + formatter.format(SIMPLE_RELEVANCE / TESTSIZE), 11) + padRight(" RLDCGCCAPPED: " + formatter.format(DCGRELEVANCECAPPED/TESTSIZE), 11)  + " RLDCG: " + formatter.format( DCGRELEVANCE/TESTSIZE));
+        Print("");
     }
+
+    private double calculateRelevance(String originalQ, String q, String suggestion) {
+        if(suggestion == null) {
+            return 0;
+        }
+        double relevanceChange;
+        int similar = 0;
+        while (similar < suggestion.length() && similar < originalQ.length()) {
+            if(suggestion.charAt(similar) != originalQ.charAt(similar)) {
+                break;
+            }
+            similar++;
+        }
+
+        relevanceChange = (Math.max(0, 2 * similar - suggestion.length() - q.length())/Double.sum (originalQ.length() - q.length(), 0));
+        return relevanceChange;
+    }
+
+    public void Print(String s) {
+        if(ENABLE_PRINT) {
+            System.out.println(s);
+        }
+    }
+
+    public static String padRight(String s, int n) {
+        return String.format("%-" + n + "s", s);
+    }
+
+    public static String padLeft(String s, int n) {
+        return String.format("%" + n + "s", s);
+    }
+
 
     public String CutOff(String s) {
         //TODO keep first word
